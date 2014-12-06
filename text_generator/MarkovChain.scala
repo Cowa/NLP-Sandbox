8e6a19885a4819e95afb162afe78f9bfc1efda36
Brice Thomas <brice.p.thomas@gmail.com>
2014-12-06 12:54:38 +0100

    Add Markov chains generator

--- text_generator/MarkovChain.scala
+++ text_generator/MarkovChain.scala
@@ -1,26 +1,53 @@
 import math.random
 
-case class MarkovChain(states: List[State]) {
-  var currentState: State = states(0)
+case class MarkovChain(chains: List[(State, List[(State, Double)])]) {
 
-  def generate(length: Int): String = {
-    currentState = initialState
-    "a"
+  /** Generate a text with the given length
+    */
+  def generate(length: Int, acc: String = ""): String = {
+    def rec(length: Int, state: State, acc: String = ""): String = length match {
+      case 0 => acc
+      case _ => rec(length - 1, next(state), acc + generateOne(state))
+    }
+    rec(length, initial())
   }
 
-  /** Return the initial state based on the entrace probabilities */
-  def initialState: State = {
-    var acc = 0.0
-    val probability = random
-
-    for (state <- states) {
-      acc += state.entrance
-
-      if (acc >= probability)
-      return state
+  /** Generate a single element
+    */
+  def generateOne(state: State, prob: Double = random, acc: Double = 0.0): String = {
+    def rec(elements: List[Element], prob: Double, acc: Double): String = elements match {
+      case h :: t if (acc + h.prob >= prob) => h.content
+      case h :: t => rec(t, prob, acc + h.prob)
+      case Nil => ""
     }
-    states(0)
+    rec(state.alphabet.elements, prob, acc)
+  }
+
+  /** Return the initial state based on the entrace probabilities
+    */
+  def initial(acc: Double = 0.0, prob: Double = random): State = {
+    def rec(chains: List[(State, List[(State, Double)])], acc: Double, prob: Double): State = chains match {
+      case (s, l) :: t if (acc + s.prob >= prob) => s
+      case (s, l) :: t => rec(t, acc + s.prob, prob)
+    }
+    rec(chains, acc, prob)
+  }
+
+  /** Return the next state of the given state
+    */
+  def next(state: State, prob: Double = random, acc: Double = 0.0): State = {
+    def rec0(l: List[(State, List[(State, Double)])]): State = l match {
+      case (s, l) :: t if (s == state) => {
+        def rec1(l: List[(State, Double)], prob: Double, acc: Double): State = l match {
+          case (s, p) :: t if (acc + p >= prob) => s
+          case (s, p) :: t => rec1(t, prob, acc + p)
+        }
+        rec1(l, prob, acc)
+      }
+      case (s, l) :: t => rec0(t)
+    }
+    rec0(chains)
   }
 }
 
-case class State(alphabet: Alphabet, entrance: Double)
+case class State(alphabet: Alphabet, prob: Double)
--- text_generator/TextGenerator.scala
+++ text_generator/TextGenerator.scala
@@ -2,14 +2,16 @@ import math.random
 
 case class TextGenerator(alphabet: Alphabet) {
 
-  /** Generate a text with the given length */
+  /** Generate a text with the given length
+    */
   def generate(length: Int, acc: String = ""): String = length match {
     case 0 => acc
-    case _ => generate(length - 1, acc + generateChar())
+    case _ => generate(length - 1, acc + generateOne())
   }
 
-  /** Generate a single element */
-  def generateChar(acc: Double = 0.0, prob: Double = random): String = {
+  /** Generate a single element
+    */
+  def generateOne(acc: Double = 0.0, prob: Double = random): String = {
     def rec(elements: List[Element], prob: Double, acc: Double): String = elements match {
       case h :: t if (acc + h.prob >= prob) => h.content
       case h :: t => rec(t, prob, acc + h.prob)
