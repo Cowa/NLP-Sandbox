6b83849a5ea09a4a749b59447b1efce72464c611
Brice Thomas <brice.p.thomas@gmail.com>
2015-03-18 14:42:15 +0100

    Working on figures extraction

--- pdf_processing/src/main/scala/pdfprocessor/PdfProcessor.scala
+++ pdf_processing/src/main/scala/pdfprocessor/PdfProcessor.scala
@@ -27,14 +27,14 @@ object PdfProcessor {
     val extracted = stripper.getText(pdf)
     pdf.close()
 
-    val (paragraphs, trash) = extractParagraphs(extracted)
+    val (paragraphs, trashes) = extractParagraphs(extracted)
 
-    println(trash.mkString("\n\n\n"))
+    //println(trashes.mkString("\n\n\n"))
+
+    extractFigures(trashes)
   }
 
-  /** [Baseline]
-    *
-    * Attempt to extract paragraphs of a PDF extracted text
+  /** Attempt to extract paragraphs of a PDF extracted text
     *
     * @param text Text extracted from a PDF (using PDFBox)
     *
@@ -45,32 +45,82 @@ object PdfProcessor {
   def extractParagraphs(text: String): (List[String], List[String]) = {
     var paragraph = ""
     var maxLineLength = 0
-    var trash = List[String]()
+    var trash = ""
+    var trashes = List[String]()
     var paragraphs = List[String]()
+    var inTrash = false
 
     text.split("\n").zipWithIndex foreach { case(line, i) =>
       line.length match {
         case x if (x >= maxLineLength) => {
           maxLineLength = x
           paragraph = paragraph + "\n" + line
+          if (inTrash) {
+            if (trash != "") trashes = trashes :+ trash
+            trash = ""
+            inTrash = false
+          }
         }
-        case x => line.trim.endsWith(".") match {
+        case x => line.trim.endsWith(".") || line.trim.endsWith(":") match {
           case true => {
             paragraphs = paragraphs :+ (paragraph + "\n" + line)
             paragraph = ""
+            if (inTrash) {
+              if (trash != "") trashes = trashes :+ trash
+              trash = ""
+              inTrash = false
+            }
           }
           case false if (x < maxLineLength / 1.75) => {
-            trash = trash :+ line
+            inTrash = true
+            trash = trash + "\n" + line
             if (paragraph != "") paragraphs = paragraphs :+ paragraph
             paragraph = ""
           }
           case false => {
             paragraph = paragraph + "\n" + line
+            if (inTrash) {
+              if (trash != "") trashes = trashes :+ trash
+              trash = ""
+              inTrash = false
+            }
           }
         }
       }
     }
 
-    (paragraphs, trash)
+    (paragraphs, trashes)
+  }
+
+  /** Attempt to extract figures from extractParagraphs' trashes
+    *
+    * @param data Trashes from extractParagraphs
+    *
+    * @return A tuple:
+    *           - List of string containing figures
+    *           - List of string containing trashes (yeah trashes from trashes!)
+    */
+  def extractFigures(data: List[String]): (List[String], List[String]) = {
+    var figures = List[String]()
+    var trashes = List[String]()
+    var figure = ""
+    var trash = ""
+
+    val isATitle = "([\\d][.].*)".r
+
+    data.foreach { d => d.split("\n") foreach { line => line match {
+      case isATitle(c) => {
+        trashes = trashes :+ line
+        if (figure != "") figures = figures :+ figure
+        figure = ""
+      }
+      case _ => {
+        figure = figure + "\n" + line
+      }
+    }}}
+
+    println(figures.mkString("\n\n\n"))
+
+    (figures, trashes)
   }
 }
