3e98892c308063ed81e754df6eec17a59b88f618
Brice Thomas <brice.p.thomas@gmail.com>
2015-03-25 22:27:47 +0100

    Add staf support for paragraphs & trashes

--- pdf_processing/src/main/scala/pdfprocessor/PdfProcessor.scala
+++ pdf_processing/src/main/scala/pdfprocessor/PdfProcessor.scala
@@ -2,18 +2,13 @@ package com.github.cowa.pdfprocessor
 
 import java.io.File;
 
-import opennlp.tools.tokenize.TokenizerME
-import opennlp.tools.tokenize.TokenizerModel
-
 import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.util.PDFTextStripper
 
+import com.github.cowa.staf._
+
 /** Process stuff on PDF, more or less */
 object PdfProcessor {
-
-  lazy val tokenizer = new TokenizerME(new TokenizerModel(
-    getClass.getResourceAsStream("/models/en-token.bin")
-  ))
 
   /** This. is. main !
     *
@@ -27,11 +22,30 @@ object PdfProcessor {
     val extracted = stripper.getText(pdf)
     pdf.close()
 
-    val (paragraphs, trashes) = extractParagraphs(extracted)
+    println(extracted)
 
-    //println(trashes.mkString("\n\n\n"))
+    val (paragraphs, trashes, stafs) = extractParagraphs(extracted)
 
-    extractFigures(trashes)
+
+    println(stafs.reverse.mkString("\n\n\n"))
+
+    val revStafs = stafs.reverse
+    val stafsParagraphs = revStafs.filter(_.`type` == "paragraph")
+    val stafsTrashs = revStafs.filter(_.`type` == "trash")
+/*
+    for (i <- 0 until paragraphs.length) {
+      println(stafsParagraphs(i))
+      println(paragraphs(i))
+      println("\n\n\n")
+    }
+*/
+    for (i <- 0 until trashes.length) {
+      println(stafsTrashs(i))
+      println(trashes(i))
+      println("\n\n\n")
+    }
+
+    //extractFigures(trashes)
   }
 
   /** Attempt to extract paragraphs of a PDF extracted text
@@ -42,7 +56,10 @@ object PdfProcessor {
     *           - string list containing each paragraph (or almost)
     *           - string list with 'kind-of' trash #happyface
     */
-  def extractParagraphs(text: String): (List[String], List[String]) = {
+  def extractParagraphs(text: String): (List[String], List[String], List[Staf]) = {
+    // So...
+    //    Ugly !
+    //        Sorry...
     var paragraph = ""
     var maxLineLength = 0
     var trash = ""
@@ -50,46 +67,78 @@ object PdfProcessor {
     var paragraphs = List[String]()
     var inTrash = false
 
+    var stafs = List[Staf]()
+    var startLine = 0
+
+    var startLineTrash = 0
+
     text.split("\n").zipWithIndex foreach { case(line, i) =>
       line.length match {
         case x if (x >= maxLineLength) => {
           maxLineLength = x
+
+          startLine = i
+
           paragraph = paragraph + "\n" + line
           if (inTrash) {
             if (trash != "") trashes = trashes :+ trash
             trash = ""
             inTrash = false
+            stafs = stafTrash(startLineTrash, i) :: stafs
           }
         }
+        // End of paragraph
         case x => line.trim.endsWith(".") || line.trim.endsWith(":") match {
           case true => {
+            if (paragraph == "") {
+              startLine = i
+            }
+
             paragraphs = paragraphs :+ (paragraph + "\n" + line)
             paragraph = ""
+
+            // endLine = i
+            stafs = stafParagraph(startLine, i) :: stafs
+            startLine = 0
+            //endLine = 0
+
             if (inTrash) {
               if (trash != "") trashes = trashes :+ trash
               trash = ""
               inTrash = false
+              stafs = stafTrash(startLineTrash, i) :: stafs
             }
           }
-          case false if (x < maxLineLength / 1.75) => {
+          case false if (x < maxLineLength / 3) => {
             inTrash = true
+            startLineTrash = i
+
             trash = trash + "\n" + line
-            if (paragraph != "") paragraphs = paragraphs :+ paragraph
+            if (paragraph != "") {
+              stafs = stafParagraph(startLine, i) :: stafs
+              startLine = 0
+              paragraphs = paragraphs :+ paragraph
+            }
             paragraph = ""
           }
           case false => {
+            if (paragraph == "") {
+              startLine = i
+            }
+
             paragraph = paragraph + "\n" + line
             if (inTrash) {
               if (trash != "") trashes = trashes :+ trash
               trash = ""
               inTrash = false
+              stafs = stafTrash(startLineTrash, i) :: stafs
             }
           }
         }
       }
     }
 
-    (paragraphs, trashes)
+    (paragraphs, trashes, stafs)
   }
 
   /** Attempt to extract figures from extractParagraphs' trashes
@@ -122,5 +171,13 @@ object PdfProcessor {
     println(figures.mkString("\n\n\n"))
 
     (figures, trashes)
+  }
+
+  def stafParagraph(startLine: Int, endLine: Int): Staf = {
+    Staf("paragraph", None, startLine, 0, endLine, 0)
+  }
+
+  def stafTrash(startLine: Int, endLine: Int): Staf = {
+    Staf("trash", None, startLine, 0, endLine, 0)
   }
 }
